4cb0ea705840affed48f3ff0bcd952cdd8fe9ccd
jeimsbe <j.d.bernales@gmail.com>
2023-11-20 20:54:26 -0300

    JS

--- insurrecto/static/js/valida.js
+++ insurrecto/static/js/valida.js
@@ -7,16 +7,25 @@ var optionNo =  $('#optionNo');
 var ageContainer =  $('.age');
 var switchArea =  $('.switch');
 
-function moveNo(){
-    setTimeout(function() {
-        switchHandle.css("width", "96px");
-        switchHandle.css("left", "0");
-        $('.age .question').text('Insurrecto Wines');
-        $('.age .statement').text('Para navegar en el sitio debés tener la edad legal para consumir alcohol.');
-    }, 100);
-};
 
-function moveYes(){
+switchHandle.draggable({
+  axis: 'x',
+  containment: 'parent',
+  stop: function() {
+    conditionMove();
+  }
+});
+
+function conditionMove() {
+    if(parseInt(switchHandle.css('left')) <= (sizeSwitch / 2)) {
+        setTimeout(function() {
+            switchHandle.css("width", "96px");
+            switchHandle.css("left", "0");
+            $('.age .question').text('Insurrecto Wines');
+            $('.age .statement').text('Para navegar en el sitio debés tener la edad legal para consumir alcohol.');
+        }, 100);
+  }
+  else {
     setTimeout(function() {
         switchHandle.css("width", "96px");
         switchHandle.css("left", "calc(100% - 96px)");
@@ -34,29 +43,6 @@ function moveYes(){
         ageContainer.addClass('hidden');
         $('.inactive').removeClass('inactive');
     }, 1000);
-};
-
-switchHandle.draggable({
-  axis: 'x',
-  stop: function() {
-    conditionMove();
   }
-});
 
-function conditionMove() {
-    if(parseInt(switchHandle.css('left')) < (sizeSwitch / 2)) {
-        moveNo();
-    }
-
-    if(parseInt(switchHandle.css('left')) > (sizeSwitch / 2)) {
-        moveYes();
-    }
 }
-
-optionNo.click(function() {
-    moveNo();
-});
-
-optionYes.click(function() {
-    moveYes();
-});
