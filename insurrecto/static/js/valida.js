66a150e15e47499d232451b6d54132f376f9ec93
jeimsbe <j.d.bernales@gmail.com>
2023-11-20 20:59:55 -0300

    JS

--- insurrecto/static/js/valida.js
+++ insurrecto/static/js/valida.js
@@ -16,6 +16,39 @@ switchHandle.draggable({
   }
 });
 
+optionYes.click(function() {
+    setTimeout(function() {
+        switchHandle.css("width", "96px");
+        switchHandle.css("left", "calc(100% - 96px)");
+    }, 100);
+
+    window.scrollTo({
+      top: 0
+    });
+
+    setTimeout(function() {
+        ageContainer.addClass('valid');
+    }, 600);
+
+    setTimeout(function() {
+        ageContainer.addClass('hidden');
+        $('.inactive').removeClass('inactive');
+    }, 1000);
+});
+
+optionNo.click(function() {
+    setTimeout(function() {
+        switchHandle.css("width", "96px");
+        switchHandle.css("left", "0");
+        $('.age .question').text('Insurrecto Wines');
+        $('.age .statement').text('Para navegar en el sitio debés tener la edad legal para consumir alcohol.');
+    }, 100);
+});
+
+switchArea.click(function() {
+  conditionMoveSnap();
+});
+
 function conditionMove() {
     if(parseInt(switchHandle.css('left')) <= (sizeSwitch / 2)) {
         setTimeout(function() {
@@ -44,5 +77,34 @@ function conditionMove() {
         $('.inactive').removeClass('inactive');
     }, 1000);
   }
+}
 
+function conditionMoveSnap() {
+    if(parseInt(switchHandle.css('left')) == sizeSwitch) {
+        setTimeout(function() {
+            switchHandle.css("width", "96px");
+            switchHandle.css("left", "0");
+            $('.age .question').text('Insurrecto Wines');
+            $('.age .statement').text('Para navegar en el sitio debés tener la edad legal para consumir alcohol.');
+        }, 100);
+  }
+  else {
+    setTimeout(function() {
+        switchHandle.css("width", "96px");
+        switchHandle.css("left", "calc(100% - 96px)");
+    }, 100);
+
+    window.scrollTo({
+      top: 0
+    });
+
+    setTimeout(function() {
+        ageContainer.addClass('valid');
+    }, 600);
+
+    setTimeout(function() {
+        ageContainer.addClass('hidden');
+        $('.inactive').removeClass('inactive');
+    }, 1000);
+  }
 }
