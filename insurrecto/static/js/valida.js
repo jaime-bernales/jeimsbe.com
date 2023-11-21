9f138776527e86a2c71fb3706debac9719f9ada2
jeimsbe <j.d.bernales@gmail.com>
2023-11-21 09:39:23 -0300

    JS

--- insurrecto/static/js/valida.js
+++ insurrecto/static/js/valida.js
@@ -45,42 +45,8 @@ optionNo.click(function() {
     }, 100);
 });
 
-switchArea.click(function() {
-  conditionMoveSnap();
-});
-
 function conditionMove() {
     if(parseInt(switchHandle.css('left')) <= (sizeSwitch / 2)) {
-        setTimeout(function() {
-            switchHandle.css("width", "96px");
-            switchHandle.css("left", "0");
-            $('.age .question').text('Insurrecto Wines');
-            $('.age .statement').text('Para navegar en el sitio debés tener la edad legal para consumir alcohol.');
-        }, 100);
-  }
-  else {
-    setTimeout(function() {
-        switchHandle.css("width", "96px");
-        switchHandle.css("left", "calc(100% - 96px)");
-    }, 100);
-
-    window.scrollTo({
-      top: 0
-    });
-
-    setTimeout(function() {
-        ageContainer.addClass('valid');
-    }, 600);
-
-    setTimeout(function() {
-        ageContainer.addClass('hidden');
-        $('.inactive').removeClass('inactive');
-    }, 1000);
-  }
-}
-
-function conditionMoveSnap() {
-    if(parseInt(switchHandle.css('left')) == sizeSwitch) {
         setTimeout(function() {
             switchHandle.css("width", "96px");
             switchHandle.css("left", "0");
